6b7f82c53cdcff9a8023afe629f7378f84526945
Helvi <helviroman@gmail.com>
2023-11-02 14:41:13 +0200

    Add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+//AND - &&
+
+//true && true --> true
+//true && false --> falce
+//falce && true --> falce
+//falce && falce --> falce
+
+
+//OR - II (pipes)
+
+//true II true --> true
+//true II falce --> true
+//false II true --> true
+//False II false --> false
+
